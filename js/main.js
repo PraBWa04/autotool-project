e4d7139722627343b6a839feca74d3f5b0139a2d
PraBWa04 <vmatishchak04@gmail.com>
2024-11-24 23:55:53 +0100

    Added JavaScript functionality for slider transitions

--- js/main.js
+++ js/main.js
@@ -9,17 +9,17 @@ function showSlide(index) {
   });
 }
 
-function showNextSlide() {
+function nextSlide() {
   currentIndex = (currentIndex + 1) % slides.length;
   showSlide(currentIndex);
 }
 
-function showPrevSlide() {
+function prevSlide() {
   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
   showSlide(currentIndex);
 }
 
-prevButton.addEventListener("click", showPrevSlide);
-nextButton.addEventListener("click", showNextSlide);
+nextButton.addEventListener("click", nextSlide);
+prevButton.addEventListener("click", prevSlide);
 
-setInterval(showNextSlide, 5000);
+setInterval(nextSlide, 5000);
