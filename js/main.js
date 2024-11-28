a294995aa24f4411a3c90dd95e368d28f1fd2ce5
PraBWa04 <vmatishchak04@gmail.com>
2024-11-28 13:56:06 +0100

    updated header styles, and adjusted search input design

--- js/main.js
+++ js/main.js
@@ -1,41 +1,31 @@
 const slides = document.querySelectorAll(".slide");
-const sliderWrapper = document.querySelector(".slider-wrapper");
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 let currentSlide = 0;
 
 function showSlide(index) {
-  const offset = -index * 100;
-  sliderWrapper.style.transform = `translateX(${offset}%)`;
+  currentSlide = (index + slides.length) % slides.length;
   slides.forEach((slide, i) => {
-    slide.classList.remove("active");
-    if (i === index) {
-      slide.classList.add("active");
-    }
+    slide.classList.toggle("active", i === currentSlide);
   });
 }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
+nextBtn.addEventListener("click", () => {
+  showSlide(currentSlide + 1);
+});
 
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
-}
+prevBtn.addEventListener("click", () => {
+  showSlide(currentSlide - 1);
+});
 
-nextBtn.addEventListener("click", nextSlide);
-prevBtn.addEventListener("click", prevSlide);
-
-let autoSlide = setInterval(nextSlide, 5000);
+let autoSlide = setInterval(() => showSlide(currentSlide + 1), 5000);
 
 document.querySelector(".slider").addEventListener("mouseenter", () => {
   clearInterval(autoSlide);
 });
 
 document.querySelector(".slider").addEventListener("mouseleave", () => {
-  autoSlide = setInterval(nextSlide, 5000);
+  autoSlide = setInterval(() => showSlide(currentSlide + 1), 5000);
 });
 
 showSlide(currentSlide);
